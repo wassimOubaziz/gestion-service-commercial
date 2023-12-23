b620c4eda82fe5dd29cb642acc71eab12aa9fae9
wassimObaziz <wassimdjaafar5@gmail.com>
2023-12-23 18:13:51 +0100

    add message

--- routes/server.js
+++ routes/server.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("./../model/User");
 const BusinessRequest = require("./../model/BusinessRequest");
+const Message = require("./../model/Message");
 
 // GET all business requests
 router.get("/posts", async (req, res) => {
@@ -55,6 +56,47 @@ router.put("/posts/:id", async (req, res) => {
     res.status(200).json(updatedPost);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+// Route to get all the clients who send messages
+// Define the route to get the last message for each client
+router.get("/messages", async (req, res) => {
+  try {
+    // Aggregate to get the last message for each client
+    const lastMessages = await Message.aggregate([
+      {
+        $group: {
+          _id: "$userId",
+          lastMessage: { $last: "$$ROOT" },
+        },
+      },
+      {
+        $lookup: {
+          from: "users", // Use the name of the users collection
+          localField: "_id",
+          foreignField: "_id",
+          as: "client",
+        },
+      },
+      {
+        $unwind: "$client",
+      },
+      {
+        $project: {
+          message: "$lastMessage.message",
+          timestamp: "$lastMessage.timestamp",
+          userId: "$lastMessage.userId",
+          last_name: "$client.last_name",
+          email: "$client.email",
+        },
+      },
+    ]);
+
+    res.json(lastMessages);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
